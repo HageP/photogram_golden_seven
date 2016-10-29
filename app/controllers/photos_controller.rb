c8ddbf41c4da915116b492b50b8c1a9ec34dff29
Pierre Hage <phage@chicagobooth.edu>
2016-10-29 03:17:26 -0500

    new photo working

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,29 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id] })
+    # @photo_url = p.source
+    # @photo_caption = p.caption
+    # @photo_id= p.id
+  end
+
+  def edit
+  @photo = Photo.find_by({ :id => params[:id] })
+
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+  p = Photo.new
+  p.caption = params[:caption]
+  p.source = params[:url]
+  p.save
+
+  redirect_to("/photos")
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get("/", { :controller => "photos", :action => "index" })
 
   # Routes to READ photos
+  get("/photos/new",       { :controller => "photos", :action => "new_form" })
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/photos/:id/edit",  { :controller => "photos", :action => "edit" })
+  get("/create_photo",     { :controller => "photos", :action => "create_row" })
 
 end
